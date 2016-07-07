9019787c26b024389075b10e7045e3314e883e2d
Benjamin Waller <benjamin@mycontact.eu>
2016-07-07 15:22:46 +0200

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/calibre/calibre-server --with-library /config/Library --username $CALUSER --password $CALPASSWORD --url-prefix $URLPREFIX
+/opt/calibre/calibre-server --with-library /config --username $CALUSER --password $CALPASSWORD --url-prefix $URLPREFIX
