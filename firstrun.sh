9298027beadfb78fb50ef344318275a08a866413
Benjamin Waller <benjamin@mycontact.eu>
2016-07-07 15:01:25 +0200

    Create firstrun.sh

---
+++ firstrun.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if config exists. If not, copy in the sample config
+if [ -f /opt/calibre/calibre-server ]; then
+  echo "Using existing install."
+else
+  echo "Installing Clean - Apply any Custom Edits"
+  cd /opt
+  wget --no-check-certificate -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main('/opt/', True)"
+  cp -ar /downloads/calibre-edits/calibre/resources/content_server /opt/calibre/resources/
+fi
+
+/opt/calibre/calibre-server --with-library /config --username $CALUSER --password $CALPASSWORD
+
+#echo "Defining Cron job from crons.conf "
+#crontab  /opt/calibre/crons.conf
+# Start cron
+
+#echo "Launching Cron"
+#cron
+#echo "Cron Done"
+
+#/opt/calibre/calibredb add /downloads/complete/Books/ -r  --with-library /config
+#rm -rf /downloads/complete/Books/*
